2e6e6b31f5de8a3329167cf83143487e9434524c
Phan Thanh Bao <baook01234@gmail.com>
2026-01-12 22:53:12 +0700

    Seed common data for tenants

--- RestX.DAL/Context/TenantDbContext.cs
+++ RestX.DAL/Context/TenantDbContext.cs
@@ -112,8 +112,13 @@ namespace RestX.DAL.Context
         #endregion
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-            => optionsBuilder.UseSqlServer("Server=.;Database=demo_tenant;User ID=sa;Password=123;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+        {
+            
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=demo_tenant;User ID=sa;Password=123;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

4e6f84701cd43209ad386d9cb555f13c8ec17992
Minh Quang <minhquangdn27@gmail.com>
2026-01-22 20:58:33 +0700

    #RX-37: Fix BaseController

--- RestX.WebApp/Controllers/BaseControllers/BaseController.cs
+++ RestX.WebApp/Controllers/BaseControllers/BaseController.cs
@@ -1,15 +1,13 @@
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using RestX.BLL.Interfaces;
-using IExceptionHandler = RestX.BLL.Interfaces.IExceptionHandler;
 
 namespace RestX.Admin.Controllers.BaseControllers
     {
         public class BaseController : Controller
         {
-            public readonly BLL.Interfaces.IExceptionHandler exceptionHandler;
+            public readonly IExceptionHandler exceptionHandler;
 
-            public BaseController(BLL.Interfaces.IExceptionHandler exceptionHandler)
+            public BaseController(IExceptionHandler exceptionHandler)
             {
                 this.exceptionHandler = exceptionHandler;
             }

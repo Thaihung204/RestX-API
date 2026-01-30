0ecdfdd2b0f4cd31dc594d289676e5fe5ea87560
thaihung204 <hunghbtde180329@fpt.edu.vn>
2026-01-31 03:39:55 +0700

    Apply redis cache for admin

--- RestX.Admin/Program.cs
+++ RestX.Admin/Program.cs
@@ -2,11 +2,15 @@ using Hangfire;
 using Hangfire.SqlServer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using RestX.AdminDAL.Context;
+using RestX.BLL;
+using RestX.BLL.Helpers;
 using RestX.BLL.Interfaces;
 using RestX.BLL.Services;
 using RestX.DAL.Context;
 using RestX.Models.Admin;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -62,7 +66,9 @@ builder.Services.AddScoped<ITenantService, TenantService>();
 builder.Services.AddScoped<IExceptionHandler, ExceptionHandler>();
 builder.Services.AddScoped<IRedisService, RedisService>();
 builder.Services.AddIdentity<Admin, IdentityRole>().AddEntityFrameworkStores<RestxAdminContext>().AddDefaultTokenProviders();
+builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
 
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 var app = builder.Build();
 app.UseCors("MultiTenantCors");
 
--- RestX.Services/Helpers/AutoMapperProfile.cs
+++ RestX.Services/Helpers/AutoMapperProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using RestX.BLL.DataTranferObjects.Tenants;
 using RestX.Models.Enum;
 using RestX.Models.Menu;
+using RestX.Models.Tenants;
 using System.Globalization;
 
 namespace RestX.BLL.Helpers
@@ -25,6 +27,7 @@ namespace RestX.BLL.Helpers
                             .ThenBy(x => x.Id)
                             .Select(x => x.ImageUrl)
                             .FirstOrDefault()));
+            this.CreateMap<Tenant, TenantOverview>().ReverseMap();
         }
     }
 }

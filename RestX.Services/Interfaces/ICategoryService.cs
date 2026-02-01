15156546220debb1bab0e0b8cf607db789b673d4
Minh Quang <minhquangdn27@gmail.com>
2026-02-01 15:10:43 +0700

    #RX-67: Add DTO for Category

--- RestX.Services/Interfaces/ICategoryService.cs
+++ RestX.Services/Interfaces/ICategoryService.cs
@@ -1,11 +1,12 @@
-﻿using RestX.Models.Menu;
+﻿using RestX.BLL.DataTranferObjects.Category;
+using RestX.Models.Menu;
 
 namespace RestX.BLL.Interfaces
 {
     public interface ICategoryService
     {
-        Task<IEnumerable<Category>> GetAllCategories();
-        Task<Category?> GetCategoryById(Guid id);
+        Task<IEnumerable<CategoryItem>> GetAllCategories();
+        Task<CategoryItem?> GetCategoryById(Guid id);
         Task<Category> UpsertCategory(Category model);
         Task DeleteCategory(Guid id);
     }
--- RestX.Services/Services/CategoryService.cs
+++ RestX.Services/Services/CategoryService.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using RestX.BLL.DataTranferObjects.Category;
 using RestX.BLL.Extensions;
 using RestX.BLL.Interfaces;
 using RestX.Models.Menu;
@@ -10,39 +12,42 @@ namespace RestX.BLL.Services
 {
     public class CategoryService : BaseService, ICategoryService
     {
+        private readonly IMapper mapper;
         public CategoryService(
+            IMapper mapper,
             IRepository repo,
             IRedisService redisService,
             IEnumerable<ActiveTenant> tenant = null
         ) : base(repo, redisService, tenant)
         {
+            this.mapper = mapper;
         }
 
         private string GetCacheKey()
             => $"{CurrentTenant?.Id}:category";
 
-        public async Task<IEnumerable<Category>> GetAllCategories()
+        public async Task<IEnumerable<CategoryItem>> GetAllCategories()
         {
-            var categories = await this.RedisService.GetAsync<List<Category>>(GetCacheKey());
+            var categories = await RedisService.GetAsync<List<Category>>(GetCacheKey());
             if (categories == null)
             {
                 categories = (await Repo.GetAllAsync<Category>(
-                        orderBy: q => q.OrderBy(c => c.Name),
-                        includeProperties: "ParentCategory,SubCategories"
-                    )).ToList();
+                    orderBy: q => q.OrderBy(c => c.Name),
+                    includeProperties: "ParentCategory,SubCategories"
+                )).ToList();
 
-                await this.RedisService.SetAsync(GetCacheKey(), categories);
+                await RedisService.SetAsync(GetCacheKey(), categories);
             }
 
-            return categories;
+            return mapper.Map<List<CategoryItem>>(categories);
         }
-
-        public async Task<Category?> GetCategoryById(Guid id)
+        public async Task<CategoryItem?> GetCategoryById(Guid id)
         {
-            return await Repo.GetOneAsync<Category>(
+            var category = await Repo.GetOneAsync<Category>(
                 filter: c => c.Id == id,
                 includeProperties: "ParentCategory,SubCategories"
             );
+            return mapper.Map<CategoryItem>(category);
         }
 
         public async Task<Category> UpsertCategory(Category model)

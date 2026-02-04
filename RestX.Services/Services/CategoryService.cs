a1178b4e0b00d12d63a535273c87898799974393
thaihung204 <hunghbtde180329@fpt.edu.vn>
2026-02-05 01:17:38 +0700

    Add Console

--- RestX.Services/Services/CategoryService.cs
+++ RestX.Services/Services/CategoryService.cs
@@ -31,6 +31,7 @@ namespace RestX.BLL.Services
             var categories = await RedisService.GetAsync<List<Category>>(GetCacheKey());
             if (categories == null)
             {
+                Console.WriteLine("Repo:", Repo.ToString());
                 categories = (await Repo.GetAllAsync<Category>(
                     orderBy: q => q.OrderBy(c => c.Name),
                     includeProperties: "ParentCategory,SubCategories"

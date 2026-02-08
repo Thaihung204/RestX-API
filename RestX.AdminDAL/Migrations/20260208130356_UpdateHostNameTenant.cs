b638b86d8e193e4bbeb771b10f8c019b0c2d1265
Minh Quang <minhquangdn27@gmail.com>
2026-02-08 20:09:45 +0700

    #RX-29: update unique hostname

---
+++ RestX.AdminDAL/Migrations/20260208130356_UpdateHostNameTenant.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestX.AdminDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateHostNameTenant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Hostname",
+                table: "Tenants",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_Hostname",
+                table: "Tenants",
+                column: "Hostname",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tenants_Hostname",
+                table: "Tenants");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Hostname",
+                table: "Tenants",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}

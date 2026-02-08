038f53e314b376641281df62f945287c3c759c60
Minh Quang <minhquangdn27@gmail.com>
2026-02-08 12:04:26 +0700

    #RX-76,77: FormController + Helper to get Enum

---
+++ RestX.Services/Helpers/FormListHelper.cs
@@ -0,0 +1,47 @@
+﻿using RestX.BLL.DataTranferObjects.Share;
+using RestX.Models.Enum;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace RestX.BLL.Helpers;
+
+public static class FormListHelper
+{
+    public static List<SelectOption> GetListByName(string name)
+    {
+        return name.Trim().ToLowerInvariant() switch
+        {
+            // Add more enum names here
+            "table-statuses" => ConvertEnumToList(typeof(TableStatus)),
+            "dish-image-types" => ConvertEnumToList(typeof(DishImageType)),
+            "order-statuses" => ConvertEnumToList(typeof(OrderStatus))
+        };
+    }
+
+    public static List<SelectOption> ConvertEnumToList(Type type)
+    {
+        var options = new List<SelectOption>();
+
+        foreach (Enum item in Enum.GetValues(type))
+        {
+            options.Add(new SelectOption
+            {
+                Id = ((int)Enum.Parse(type, item.ToString())).ToString(),
+                Name = StringValueOfEnum(item)
+            });
+        }
+
+        return options;
+    }
+
+    public static string StringValueOfEnum(Enum value)
+    {
+        FieldInfo? fi = value.GetType().GetField(value.ToString());
+        var attributes = (DescriptionAttribute[]?)fi?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        if (attributes != null && attributes.Length > 0)
+            return attributes[0].Description;
+
+        return value.ToString();
+    }
+}
---
+++ RestX.WebApp/Controllers/FormController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestX.BLL.Helpers;
+using RestX.BLL.Interfaces;
+using RestX.WebApp.Controllers.BaseControllers;
+
+namespace RestX.WebApp.Controllers;
+
+[Route("api/forms")]
+[ApiController]
+public class FormController : BaseController
+{
+    public FormController(IExceptionHandler exceptionHandler) : base(exceptionHandler) { }
+
+    [HttpGet("get-lists/{name}")]
+    public IActionResult GetLists([FromRoute] string name)
+    {
+        try
+        {
+            var data = FormListHelper.GetListByName(name);
+            return Ok(new { success = true, data });
+        }
+        catch (Exception ex)
+        {
+            exceptionHandler.RaiseException(ex);
+            return BadRequest(new { success = false, message = "An internal error occurred" });
+        }
+    }
+}

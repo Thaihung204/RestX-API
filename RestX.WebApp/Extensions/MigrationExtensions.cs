4e97e5478e816ad9b4c49ce822582708b0233ca3
thaihung204 <hunghbtde180329@fpt.edu.vn>
2026-02-08 21:00:31 +0700

    Add migration for tenant

---
+++ RestX.WebApp/Extensions/MigrationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestX.DAL.Context;
+
+namespace RestX.WebApp.Extensions
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+
+            var db = scope.ServiceProvider
+                .GetRequiredService<TenantDbContext>();
+
+            db.Database.Migrate();
+        }
+    }
+
+}

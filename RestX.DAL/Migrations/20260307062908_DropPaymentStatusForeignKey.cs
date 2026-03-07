0f91f0f299e7095cebc7aa55da8aed6850fe0fdd
PhanThanhBao <baook01234@gmail.com>
2026-03-07 13:36:57 +0700

    Remove Enum Foreign Key

---
+++ RestX.DAL/Migrations/20260307062908_DropPaymentStatusForeignKey.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestX.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropPaymentStatusForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_StatusValues_PaymentStatusId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_PaymentStatusId",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PaymentStatusId",
+                table: "Orders",
+                column: "PaymentStatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_StatusValues_PaymentStatusId",
+                table: "Orders",
+                column: "PaymentStatusId",
+                principalTable: "StatusValues",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

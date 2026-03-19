ea02fc384bc53aef7701e42a4c1edbc3ac7ca8e8
thaihung204 <hunghbtde180329@fpt.edu.vn>
2026-03-19 22:50:28 +0700

    Hotfix AI Table

---
+++ RestX.DAL/Migrations/20260319154752_UpdateAITable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestX.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAITable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PropertiesJson",
+                table: "AIChatSessions",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PropertiesJson",
+                table: "AIChatMessages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PropertiesJson",
+                table: "AIChatSessions");
+
+            migrationBuilder.DropColumn(
+                name: "PropertiesJson",
+                table: "AIChatMessages");
+        }
+    }
+}

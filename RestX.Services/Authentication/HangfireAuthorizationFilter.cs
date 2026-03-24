0f92166d05815921ec07e6b9524706ebc87e098c
thaihung204 <hunghbtde180329@fpt.edu.vn>
2026-03-24 23:35:24 +0700

    Update Hangfire Version

--- RestX.Services/Authentication/HangfireAuthorizationFilter.cs
+++ RestX.Services/Authentication/HangfireAuthorizationFilter.cs
@@ -1,30 +1,17 @@
 ﻿using Hangfire.Dashboard;
-using Microsoft.AspNetCore.Http;
 
 namespace RestX.BLL.Authentication
 {
     public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
-        public HangfireAuthorizationFilter(IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
         public HangfireAuthorizationFilter()
         {
         }
 
         public bool Authorize(DashboardContext context)
         {
-            var httpContext = _httpContextAccessor.HttpContext;
+            var httpContext = context.GetHttpContext();
             return httpContext?.User?.Identity?.IsAuthenticated ?? false;
         }
-
-        //public bool Authorize(DashboardContext context)
-        //{
-        //    var httpContext = context.GetHttpContext();
-        //    return httpContext?.User?.Identity?.IsAuthenticated ?? false;
-        //}
     }
 }

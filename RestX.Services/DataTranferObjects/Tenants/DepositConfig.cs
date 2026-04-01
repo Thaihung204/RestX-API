e1b376af0919f419db20d629d1f0bc8de85a7ed4
PhanThanhBao <baook01234@gmail.com>
2026-04-01 16:34:12 +0700

    clean default value

--- RestX.Services/DataTranferObjects/Tenants/DepositConfig.cs
+++ RestX.Services/DataTranferObjects/Tenants/DepositConfig.cs
@@ -5,24 +5,19 @@ namespace RestX.BLL.DataTranferObjects.Tenants
     public class DepositConfig
     {
         [Range(1, 100)]
-        public int MinPartySize { get; set; } = 8;
-
+        public int MinPartySize { get; set; }
         [Range(0, 999999999)]
-        public decimal DepositAmountPerPerson { get; set; } = 50000;
-
+        public decimal DepositAmountPerPerson { get; set; }
         [Range(1, 168)]
-        public int DeadlineHours { get; set; } = 2;
+        public int DeadlineHours { get; set; }
 
         [Range(1, 720)]
-        public int EarlyRefundHours { get; set; } = 48;
-
+        public int EarlyRefundHours { get; set; }
         [Range(0, 100)]
-        public int EarlyRefundPercentage { get; set; } = 100;
-
+        public int EarlyRefundPercentage { get; set; }
         [Range(1, 720)]
-        public int LateRefundHours { get; set; } = 24;
-
+        public int LateRefundHours { get; set; }
         [Range(0, 100)]
-        public int LateRefundPercentage { get; set; } = 50;
+        public int LateRefundPercentage { get; set; }
     }
 }

44080a6ded2b7649b8bc8209a7e2ae805424a157
Minh Quang <minhquangdn27@gmail.com>
2026-04-02 01:17:36 +0700

    #Debug 1

--- RestX.Services/MultiTenancy/TenantResolver.cs
+++ RestX.Services/MultiTenancy/TenantResolver.cs
@@ -68,7 +68,7 @@ namespace RestX.BLL.MultiTenancy
                 context.Request.Headers["X-Forwarded-Host"].FirstOrDefault());
 
             hostname = hostname.ToLower();
-            Console.WriteLine("hostname: ", hostname);
+            Console.WriteLine($"hostname: {hostname}");
 
             var hostnameWithPath = "";
             try
@@ -80,7 +80,7 @@ namespace RestX.BLL.MultiTenancy
                                 context.Request.Path.Value.Substring(1).IndexOf("/", StringComparison.Ordinal))
                             : context.Request.Path.Value.Substring(1)))
                     : string.Empty;
-                Console.WriteLine("hostnameWithPath: ", hostnameWithPath);
+                Console.WriteLine($"hostnameWithPath: {hostnameWithPath}");
             }
             catch (Exception ex)
             {

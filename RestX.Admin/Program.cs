a67134ea7094d0db050833634b48e519fea4a502
PhanThanhBao <baook01234@gmail.com>
2026-04-19 00:41:25 +0700

    remove unused

--- RestX.Admin/Program.cs
+++ RestX.Admin/Program.cs
@@ -20,7 +20,6 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Json.Serialization;
 
-QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add DB Context 
